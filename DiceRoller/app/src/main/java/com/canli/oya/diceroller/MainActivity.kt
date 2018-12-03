3311ce92ce79a2db96241fc55e05b2b3608af313
Oya Canli <oyacan@gmail.com>
2018-12-03 22:42:27 +0100

    pick unrepeated numbers and refactor animateDice method for a prettier animation.

--- DiceRoller/app/src/main/java/com/canli/oya/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/canli/oya/diceroller/MainActivity.kt
@@ -3,15 +3,16 @@ package com.canli.oya.diceroller
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var dice_iv: ImageView
+    private val TAG = "MAinActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +27,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollAndAnimateDice() {
 
+        val diceRange = mutableListOf(1,2,3,4,5,6)
+        diceRange.shuffle()
+        Log.d(TAG, "diceRange: $diceRange")
+
         /*Create a local function for rolling the dice:
         Pick a random int and set the corresponding dice image*/
-        fun rollDice() {
-            val randomInt = Random().nextInt(6) + 1
-            val resourceId: Int = when (randomInt) {
+        fun rollDice(count : Int) {
+            val resourceId: Int = when (diceRange[count]) {
                 1 -> R.drawable.dice_1
                 2 -> R.drawable.dice_2
                 3 -> R.drawable.dice_3
@@ -38,28 +42,28 @@ class MainActivity : AppCompatActivity() {
                 5 -> R.drawable.dice_5
                 else -> R.drawable.dice_6
             }
+            Log.d(TAG, "picked number: ${diceRange[count]}")
             dice_iv.setImageResource(resourceId)
         }
 
         /*Create a local function for animating roll dice:
         Roll the dice three times with small pauses, then stop rolling and begin blinking animation*/
         fun animateDice(initialCount : Int){
-            //After three rolls, stop rolling and begin blinking animation
-            if(initialCount == 3){
-                val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.blinking_anim)
-                dice_iv.startAnimation(animation)
-                return
-            }
-
             //wait between each roll
             Handler().postDelayed({
-                rollDice()
-                animateDice(initialCount + 1)
+                if(initialCount < 2){
+                    rollDice(initialCount + 1)
+                    animateDice(initialCount + 1)
+                } else {
+                    //When count reaches 2, stop rolling a start blinking animation
+                    val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.blinking_anim)
+                    dice_iv.startAnimation(animation)
+                }
             }, 300)
         }
 
         //Make a initial roll and then begin rolling with time gaps
-        rollDice()
-        animateDice(1)
+        rollDice(0)
+        animateDice(0)
     }
 }
