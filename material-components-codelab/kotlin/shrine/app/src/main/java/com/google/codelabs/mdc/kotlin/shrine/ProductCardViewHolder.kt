fc137e705774e56feb860ca27469c0e21d454483
Oya Canli <oyacan@gmail.com>
2019-01-09 00:13:09 +0100

    mdc 103 : customize style

--- material-components-codelab/kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++ material-components-codelab/kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
@@ -5,8 +5,7 @@ import android.view.View
 import android.widget.TextView
 import com.android.volley.toolbox.NetworkImageView
 
-class ProductCardViewHolder(itemView: View)
-    : RecyclerView.ViewHolder(itemView){
+class ProductCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
     var productImage: NetworkImageView = itemView.findViewById(R.id.product_image)
     var productTitle: TextView = itemView.findViewById(R.id.product_title)
