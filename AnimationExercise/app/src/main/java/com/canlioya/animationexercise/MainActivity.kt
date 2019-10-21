97127280d7205fbf8ccfd8afbbfe2861450692f7
OyaCanli <oyacan@gmail.com>
2019-10-21 10:56:25 +0200

    animation exercise: try animators from resources

--- AnimationExercise/app/src/main/java/com/canlioya/animationexercise/MainActivity.kt
+++ AnimationExercise/app/src/main/java/com/canlioya/animationexercise/MainActivity.kt
@@ -1,30 +1,41 @@
 package com.canlioya.animationexercise
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var angle = 10f
+    val animatorSet : AnimatorSet by lazy {
+        AnimatorInflater.loadAnimator(this@MainActivity, R.animator.rotate) as AnimatorSet}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            runRotateAnimation()
-        }
-    }
-
-    private fun runRotateAnimation() {
-        angle *= -1
-        textView.animate()
-            .rotationBy(angle)
-            .setDuration(100)
-            .withEndAction{
-                runRotateAnimation()
+        animatorSet.duration = 2000
+        animatorSet.setTarget(ball_image)
+        animatorSet.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) {
+                Toast.makeText(this@MainActivity, "Animation started", Toast.LENGTH_SHORT).show()
             }
-    }
+            override fun onAnimationRepeat(p0: Animator?) {
+                Toast.makeText(this@MainActivity, "Animation is repeated", Toast.LENGTH_SHORT).show()
+            }
+            override fun onAnimationCancel(p0: Animator?) {}
 
+            override fun onAnimationEnd(p0: Animator?) {
+                Toast.makeText(this@MainActivity, "Animation ended", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        button.setOnClickListener {
+            animatorSet.start()
+        }
+
+    }
 }
