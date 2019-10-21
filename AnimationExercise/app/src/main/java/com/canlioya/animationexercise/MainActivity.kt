fe3bc2f6e9f2a218e068e6abac0a3b807442cf19
OyaCanli <oyacan@gmail.com>
2019-10-21 12:05:20 +0200

    animation exercise: try transition sets

--- AnimationExercise/app/src/main/java/com/canlioya/animationexercise/MainActivity.kt
+++ AnimationExercise/app/src/main/java/com/canlioya/animationexercise/MainActivity.kt
@@ -4,13 +4,14 @@ import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.os.Bundle
+import android.transition.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val animatorSet : AnimatorSet by lazy {
+    private val animatorSet : AnimatorSet by lazy {
         AnimatorInflater.loadAnimator(this@MainActivity, R.animator.rotate) as AnimatorSet}
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             override fun onAnimationCancel(p0: Animator?) {}
 
             override fun onAnimationEnd(p0: Animator?) {
-                Toast.makeText(this@MainActivity, "Animation ended", Toast.LENGTH_SHORT).show()
+                transitionToSecondLayout()
             }
         })
 
@@ -37,5 +38,26 @@ class MainActivity : AppCompatActivity() {
             animatorSet.start()
         }
 
+    }
+
+    private fun transitionToSecondLayout() {
+        val fade : Transition = Fade()
+        val changeBounds : Transition = ChangeBounds()
+        val transitionSet = TransitionSet()
+        transitionSet.addTransition(fade)
+            .addTransition(changeBounds)
+            .setDuration(2000)
+            .addListener(object: Transition.TransitionListener{
+                override fun onTransitionEnd(p0: Transition?) {
+                    Toast.makeText(this@MainActivity, "Transition ended", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onTransitionResume(p0: Transition?) {}
+                override fun onTransitionPause(p0: Transition?) {}
+                override fun onTransitionCancel(p0: Transition?) {}
+                override fun onTransitionStart(p0: Transition?) {}
+            })
+        val scene2 = Scene.getSceneForLayout(root_main, R.layout.layout_second, this)
+        TransitionManager.go(scene2, transitionSet)
     }
 }
