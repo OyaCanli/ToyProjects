1dd96ba2367084c308f8cdd0a7a56f492b050849
OyaCanli <oyacan@gmail.com>
2019-10-21 10:16:30 +0200

    animation exercise: try view property animation

--- AnimationExercise/app/src/main/java/com/canlioya/animationexercise/MainActivity.kt
+++ AnimationExercise/app/src/main/java/com/canlioya/animationexercise/MainActivity.kt
@@ -1,52 +1,30 @@
 package com.canlioya.animationexercise
 
-import android.animation.AnimatorSet
-import android.animation.ArgbEvaluator
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val xScaleAnimator : ObjectAnimator by lazy {
-        ObjectAnimator.ofFloat(textView, View.SCALE_X, 2f)
-    }
-
-    private val yScaleAnimator : ObjectAnimator by lazy {
-        ObjectAnimator.ofFloat(textView, View.SCALE_Y, 2f)
-    }
-
-    private val colorAnimator : ObjectAnimator by lazy {
-        ObjectAnimator.ofObject(textView, "textColor", ArgbEvaluator(), Color.WHITE, Color.GREEN)
-    }
-
-    private val yTranslateAnimator : ObjectAnimator by lazy {
-        ObjectAnimator.ofFloat(textView, View.TRANSLATION_Y, 50f)
-    }
+    var angle = 10f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setRepeatModeOnAnimator(xScaleAnimator, yScaleAnimator, colorAnimator, yTranslateAnimator)
-
-        val animatorSet = AnimatorSet()
-        animatorSet.playTogether(xScaleAnimator, yScaleAnimator, colorAnimator, yTranslateAnimator)
-        animatorSet.duration = 1000
-
-        button.setOnClickListener { animatorSet.start() }
+        button.setOnClickListener {
+            runRotateAnimation()
+        }
     }
 
-    private fun setRepeatModeOnAnimator(vararg animators : ObjectAnimator) {
-        for(animator in animators) {
-            animator.repeatCount = 5
-            animator.repeatMode = ValueAnimator.REVERSE
-        }
-
+    private fun runRotateAnimation() {
+        angle *= -1
+        textView.animate()
+            .rotationBy(angle)
+            .setDuration(100)
+            .withEndAction{
+                runRotateAnimation()
+            }
     }
 
 }
