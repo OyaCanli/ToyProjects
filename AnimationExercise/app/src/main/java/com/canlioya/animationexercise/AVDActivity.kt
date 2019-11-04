8c91bf1fc886155d39b8c64905c14a50ef78d8f2
OyaCanli <oyacan@gmail.com>
2019-11-04 11:14:53 +0100

    animation exercise: add plus-to-cross animated vector drawable

--- AnimationExercise/app/src/main/java/com/canlioya/animationexercise/AVDActivity.kt
+++ AnimationExercise/app/src/main/java/com/canlioya/animationexercise/AVDActivity.kt
@@ -1,20 +1,51 @@
 package com.canlioya.animationexercise
 
+import android.graphics.drawable.Animatable2
 import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_avds.*
 
 class AVDActivity : AppCompatActivity() {
 
+    var inCrossShape = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_avds)
 
-        val tomatoAvd : AnimatedVectorDrawable = tomato_image.drawable as AnimatedVectorDrawable
-        animate_tomato.setOnClickListener {  tomatoAvd.start() }
+        val tomatoAvd: AnimatedVectorDrawable = tomato_image.drawable as AnimatedVectorDrawable
+        animate_tomato.setOnClickListener { tomatoAvd.start() }
 
-        val ballAvd : AnimatedVectorDrawable = ball_image.drawable as AnimatedVectorDrawable
-        animate_ball.setOnClickListener {  ballAvd.start() }
+        val ballAvd: AnimatedVectorDrawable = ball_image.drawable as AnimatedVectorDrawable
+        animate_ball.setOnClickListener { ballAvd.start() }
+
+        plus_to_cross_btn.setOnClickListener {
+            val plusToCrossAvd: AnimatedVectorDrawable =
+                plus_image.drawable as AnimatedVectorDrawable
+            plusToCrossAvd.clearAnimationCallbacks()
+            plusToCrossAvd.registerAnimationCallback(AnimOverCallback())
+            plusToCrossAvd.start()
+        }
+    }
+
+    fun changeImageDrawable() {
+        if (!inCrossShape) {
+            inCrossShape = true
+            plus_image.setImageResource(R.drawable.cross_to_plus_avd)
+        } else {
+            inCrossShape = false
+            plus_image.setImageResource(R.drawable.plus_to_cross_avd)
+        }
+    }
+
+    inner class AnimOverCallback : Animatable2.AnimationCallback() {
+        override fun onAnimationStart(drawable: Drawable) {}
+
+        override fun onAnimationEnd(drawable: Drawable) {
+            changeImageDrawable() }
     }
 }
+
+
