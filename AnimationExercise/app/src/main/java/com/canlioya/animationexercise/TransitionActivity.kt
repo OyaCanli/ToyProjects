9e2e191149892ccdddd7263483973ed09b2a02dc
OyaCanli <oyacan@gmail.com>
2020-07-10 12:14:44 +0200

    animation exercise : add flip card animation

--- AnimationExercise/app/src/main/java/com/canlioya/animationexercise/TransitionActivity.kt
+++ AnimationExercise/app/src/main/java/com/canlioya/animationexercise/TransitionActivity.kt
@@ -3,6 +3,7 @@ package com.canlioya.animationexercise
 import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
+import android.graphics.Color
 import android.os.Bundle
 import android.transition.Scene
 import android.transition.TransitionInflater
@@ -29,6 +30,8 @@ class TransitionActivity : AppCompatActivity() {
     private val animatorSet : AnimatorSet by lazy {
         AnimatorInflater.loadAnimator(this@TransitionActivity, R.animator.rotate) as AnimatorSet}
 
+    private var cardFlipped = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transition)
@@ -53,6 +56,39 @@ class TransitionActivity : AppCompatActivity() {
         button.setOnClickListener {
             animatorSet.start()
         }
+
+        flip_image.setOnClickListener { flipImage() }
+    }
+
+    private fun flipImage() {
+        val animatorFlipLeftOut = AnimatorInflater.loadAnimator(this@TransitionActivity, R.animator.flip_left_out)
+        animatorFlipLeftOut.setTarget(flip_image)
+
+        val animatorFlipRightIn = AnimatorInflater.loadAnimator(this@TransitionActivity, R.animator.flip_left_in)
+        animatorFlipRightIn.setTarget(flip_image)
+
+        animatorFlipLeftOut.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) {
+                Toast.makeText(this@TransitionActivity, "Animation started", Toast.LENGTH_SHORT).show()
+            }
+            override fun onAnimationRepeat(p0: Animator?) {
+                Toast.makeText(this@TransitionActivity, "Animation is repeated", Toast.LENGTH_SHORT).show()
+            }
+            override fun onAnimationCancel(p0: Animator?) {}
+
+            override fun onAnimationEnd(p0: Animator?) {
+                if(cardFlipped){
+                    flip_image.setBackgroundResource(R.drawable.maiden_tower)
+                    flip_image.text = ""
+                } else {
+                    flip_image.setBackgroundColor(Color.GRAY)
+                    flip_image.text = "MAIDEN TOWER"
+                }
+                animatorFlipRightIn.start()
+                cardFlipped = !cardFlipped
+            }
+        })
+        animatorFlipLeftOut.start()
     }
 
     override fun onBackPressed() {
