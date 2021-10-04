3a3a6f1d844ecd8c6892a7ad6ddc4f07f7c79d32
OyaCanli <oyacan@gmail.com>
2021-10-04 12:58:55 +0200

    apollo-android : subscribe to changes

--- apollo-android-sample/app/src/main/java/com/example/rocketreserver/Apollo.kt
+++ apollo-android-sample/app/src/main/java/com/example/rocketreserver/Apollo.kt
@@ -3,6 +3,7 @@ package com.example.rocketreserver
 import android.content.Context
 import android.os.Looper
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -19,13 +20,15 @@ fun apolloClient(context: Context): ApolloClient {
         return instance!!
     }
 
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(AuthorizationInterceptor(context))
+        .build()
+
+    // wss:// is the protocol for WebSocket.
     instance = ApolloClient.builder()
-        .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/")
-        .okHttpClient(
-            OkHttpClient.Builder()
-                .addInterceptor(AuthorizationInterceptor(context))
-                .build()
-        )
+        .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+        .subscriptionTransportFactory(WebSocketSubscriptionTransport.Factory("wss://apollo-fullstack-tutorial.herokuapp.com/graphql", okHttpClient))
+        .okHttpClient(okHttpClient)
         .build()
 
     return instance!!
