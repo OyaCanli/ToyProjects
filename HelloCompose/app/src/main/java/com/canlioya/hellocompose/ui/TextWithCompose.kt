792a1229df8b45c71875b6202af87287a700bf32
OyaCanli <oyacan@gmail.com>
2021-10-08 10:35:41 +0200

    hello-compose : add spanned text example

--- HelloCompose/app/src/main/java/com/canlioya/hellocompose/ui/TextWithCompose.kt
+++ HelloCompose/app/src/main/java/com/canlioya/hellocompose/ui/TextWithCompose.kt
@@ -1,20 +1,22 @@
 package com.canlioya.hellocompose.ui
 
-
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,5 +56,31 @@ fun MyEditText() {
         },
         singleLine = true,
         modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SpannedString() {
+    val text = buildAnnotatedString {
+        withStyle(SpanStyle(color = Color.Red)) {
+            append("H")
+        }
+        withStyle(SpanStyle(color = Color.Yellow)) {
+            append("E")
+        }
+        withStyle(SpanStyle(color = Color.Green)) {
+            append("L")
+        }
+        withStyle(SpanStyle(color = Color.Magenta)) {
+            append("L")
+        }
+        withStyle(SpanStyle(color = Color.Blue)) {
+            append("O")
+        }
+    }
+    Text(
+        text = text,
+        modifier = Modifier.padding(8.dp)
     )
 }
